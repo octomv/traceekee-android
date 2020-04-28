b72eb10aaef385b7aec13347369a0b2d52e24d74
kudanai <kudanai@gmail.com>
2020-04-28 15:10:47 +0800

    layout changes version bump

--- app/src/main/java/io/bluetrace/opentrace/onboarding/OTPFragment.kt
+++ app/src/main/java/io/bluetrace/opentrace/onboarding/OTPFragment.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import androidx.core.text.BidiFormatter
 import androidx.core.text.HtmlCompat
 import kotlinx.android.synthetic.main.fragment_otp.*
 import io.bluetrace.opentrace.Preference
